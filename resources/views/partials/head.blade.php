f00f79cf000ace645483a2132ca0a43af3f31734
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-11-22 00:19:43 +0600

    mastering for frontend ..

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Themezhub" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kumo- Fashion eCommerce HTML Template</title>
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('assets/css/plugins/animation.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/flaticon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/iconfont.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/ion.rangeSlider.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/light-box.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/line-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/slick-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/slick.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/snackbar.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/plugins/themify.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+
+</head>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/shop', [ShopController::class, 'index'])->name('shop');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
