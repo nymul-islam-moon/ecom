d4a6ca13ef5530afd1c30a7ef5b860e6d3dcd0de
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-11-25 21:35:23 +0600

    project optimized

--- app/Http/Controllers/Admin/TestController.php
+++ app/Http/Controllers/Admin/TestController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         dd('hello');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ShopController;
 use App\Http\Controllers\Admin\SizeController;
 use App\Http\Controllers\Admin\SubCategoryController;
-use App\Http\Controllers\Admin\TestController;
 use Illuminate\Support\Facades\Route;
 
 // Route::resource('/shop', ShopController::class);
@@ -16,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('size', SizeController::class);
 // Route::resource('/category', CategoryController::class);
 // Route::get('/category', [CategoryController::class, 'index'])->name('admin.test');
-
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
