dc05cf0833373059ef512376156bb6ec222550a9
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-11-25 22:54:01 +0600

    remove unused codes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,16 +2,11 @@
 
 namespace App\Models;
 
-use App\Models\SubCategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'status'];
-
-    public function subCategories()
-    {
-        return $this->hasMany(SubCategory::class);
-    }
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
 }
