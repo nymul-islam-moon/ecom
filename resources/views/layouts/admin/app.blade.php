2a8d3b736d52d45d7706810fb551373d20c88bbd
Nymul Islam <nymul@worldtechsoft.com>
2024-11-26 15:51:34 +0600

    blade mastering done

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1,30 +1,4 @@
-<!doctype html>
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
-    data-sidebar-image="none" data-preloader="disable">
-
-<head>
-
-    <meta charset="utf-8" />
-    <title>Starter | Velzon - Admin & Dashboard Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
-
-    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.ico') }}">
-
-
-    <script src="{{ asset('admin/js/layout.js') }}"></script>
-
-    <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <link href="{{ asset('admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <link href="{{ asset('admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <link href="{{ asset('admin/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
-
-</head>
-
+@include('partials.admin.head')
 <body>
 
     <!-- Begin page -->
@@ -3109,19 +3083,6 @@
         </div>
     </div>
 
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('admin/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('admin/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset('admin/libs/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('admin/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-    <script src="{{ asset('admin/js/plugins.js') }}"></script>
-
-    <!-- App js -->
-    <script src="{{ asset('admin/js/app.js') }}"></script>
+    @include('partials.admin.scripts')
 </body>
-
-
-<!-- Mirrored from themesbrand.com/velzon/html/default/pages-starter.html by HTTrack Website Copier/3.x [XR&CO'2010], Wed, 22 Mar 2023 05:43:08 GMT -->
-
 </html>
---
+++ resources/views/partials/admin/head.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
+    data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Starter | Velzon - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+
+    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.ico') }}">
+
+
+    <script src="{{ asset('admin/js/layout.js') }}"></script>
+
+    <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <link href="{{ asset('admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <link href="{{ asset('admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <link href="{{ asset('admin/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
