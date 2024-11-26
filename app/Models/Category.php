93e5532b438892c25dc84ee08448b7ff04bb1d89
Nymul Islam <nymul@worldtechsoft.com>
2024-11-26 18:05:50 +0600

    all creating

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'status'];
-
-    public function subCategories()
-    {
-        return $this->hasMany(SubCategory::class);
-    }
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
 }
