0e1082ea9534e42120754c6e22bb84545519db39
shamsalm0 <shamsalm0@gmail.com>
2024-12-02 21:29:54 +0600

    color crud

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['web']], function () {
     Route::resource('/category', CategoryController::class);
     Route::resource('/brand', BrandController::class);
+    Route::resource('/colors',ColorController::class);
 });
 // Route::resource('subcategory', SubCategoryController::class);
 // Route::resource('color', ColorController::class);
