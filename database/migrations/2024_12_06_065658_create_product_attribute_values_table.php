9ef779e7ad45909b3103e3417cd67458f0ff9844
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-12-06 13:06:05 +0600

    migration upgraded

--- database/migrations/2024_12_06_065658_create_product_attribute_values_table.php
+++ database/migrations/2024_12_06_065658_create_product_attribute_values_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
     {
         Schema::create('product_attribute_values', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('product_id'); // Foreign key for product
-            // $table->unsignedBigInteger('attribute_value_id'); // Foreign key for attribute value
-            // $table->primary(['product_id', 'attribute_value_id']); // Composite primary key
-
-            // // Foreign key constraints
-            // $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade');
-            // $table->foreign('attribute_value_id')->references('attribute_value_id')->on('attribute_values')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('attribute_value_id')->constrained('attribute_values')->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['product_id', 'attribute_value_id']);
         });
     }
 
