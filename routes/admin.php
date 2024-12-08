9c150207f245f33eacc439ed31942854fd361a19
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-12-08 19:31:03 +0600

    route updated

--- routes/admin.php
+++ routes/admin.php
@@ -26,8 +26,8 @@ Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::resource('/brand', BrandController::class);
-Route::resource('/category', CategoryController::class);
+Route::resource('brand', BrandController::class);
+Route::resource('category', CategoryController::class);
 Route::resource('sub-category', SubCategoryController::class);
 Route::resource('customer', CustomerController::class);
 // });
