866bcd91fb11dd7c46490cba7d924ef404aa5905
Nymul Islam <nymul@worldtechsoft.com>
2024-12-08 17:56:50 +0600

    customer list and sidebar updated

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1629,13 +1629,13 @@
                         <li class="nav-item">
                             <a class="nav-link menu-link" href="#sidebarPages" data-bs-toggle="collapse"
                                 role="button" aria-expanded="false" aria-controls="sidebarPages">
-                                <i class="ri-pages-line"></i> <span data-key="t-pages">Pages</span>
+                                <i class="ri-pages-line"></i> <span data-key="t-pages">User Management</span>
                             </a>
                             <div class="collapse menu-dropdown" id="sidebarPages">
                                 <ul class="nav nav-sm flex-column">
                                     <li class="nav-item">
-                                        <a href="pages-starter.html" class="nav-link" data-key="t-starter">
-                                            Starter </a>
+                                        <a href="{{ route('admin.customer.index') }}" class="nav-link" data-key="t-starter">
+                                            Customers </a>
                                     </li>
                                     <li class="nav-item">
                                         <a href="#sidebarProfile" class="nav-link" data-bs-toggle="collapse"
