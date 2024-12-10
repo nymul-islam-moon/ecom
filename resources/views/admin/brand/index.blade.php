d655318e3c3a9f6d8d2be7a762081e1df111c6bd
Nymul Islam <nymul@worldtechsoft.com>
2024-12-10 10:06:34 +0600

    image shown done

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -36,12 +36,22 @@
                     </div>
                     <div class="d-flex align-items-end justify-content-between mt-4">
                         <div>
-                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="559.25">559.25</span>k</h4>
+                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
+                                    data-target="559.25">559.25</span>k</h4>
                             <span class="badge bg-warning me-1">2,258</span> <span class="text-muted">Brands sent</span>
                         </div>
                         <div class="avatar-sm flex-shrink-0">
                             <span class="avatar-title bg-light rounded fs-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text text-success icon-dual-success"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="feather feather-file-text text-success icon-dual-success">
+                                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                    <polyline points="14 2 14 8 20 8"></polyline>
+                                    <line x1="16" y1="13" x2="8" y2="13"></line>
+                                    <line x1="16" y1="17" x2="8" y2="17"></line>
+                                    <polyline points="10 9 9 9 8 9"></polyline>
+                                </svg>
                             </span>
                         </div>
                     </div>
@@ -65,12 +75,19 @@
                     </div>
                     <div class="d-flex align-items-end justify-content-between mt-4">
                         <div>
-                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="409.66">409.66</span>k</h4>
+                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
+                                    data-target="409.66">409.66</span>k</h4>
                             <span class="badge bg-warning me-1">1,958</span> <span class="text-muted">Paid by clients</span>
                         </div>
                         <div class="avatar-sm flex-shrink-0">
                             <span class="avatar-title bg-light rounded fs-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square text-success icon-dual-success"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="feather feather-check-square text-success icon-dual-success">
+                                    <polyline points="9 11 12 14 22 4"></polyline>
+                                    <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+                                </svg>
                             </span>
                         </div>
                     </div>
@@ -94,12 +111,18 @@
                     </div>
                     <div class="d-flex align-items-end justify-content-between mt-4">
                         <div>
-                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="136.98">136.98</span>k</h4>
+                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
+                                    data-target="136.98">136.98</span>k</h4>
                             <span class="badge bg-warning me-1">338</span> <span class="text-muted">Unpaid by clients</span>
                         </div>
                         <div class="avatar-sm flex-shrink-0">
                             <span class="avatar-title bg-light rounded fs-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock text-success icon-dual-success"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-clock text-success icon-dual-success">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <polyline points="12 6 12 12 16 14"></polyline>
+                                </svg>
                             </span>
                         </div>
                     </div>
@@ -123,12 +146,23 @@
                     </div>
                     <div class="d-flex align-items-end justify-content-between mt-4">
                         <div>
-                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="84.20">84.2</span>k</h4>
-                            <span class="badge bg-warning me-1">502</span> <span class="text-muted">Cancelled by clients</span>
+                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
+                                    data-target="84.20">84.2</span>k</h4>
+                            <span class="badge bg-warning me-1">502</span> <span class="text-muted">Cancelled by
+                                clients</span>
                         </div>
                         <div class="avatar-sm flex-shrink-0">
                             <span class="avatar-title bg-light rounded fs-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-octagon text-success icon-dual-success"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="feather feather-x-octagon text-success icon-dual-success">
+                                    <polygon
+                                        points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2">
+                                    </polygon>
+                                    <line x1="15" y1="9" x2="9" y2="15"></line>
+                                    <line x1="9" y1="9" x2="15" y2="15"></line>
+                                </svg>
                             </span>
                         </div>
                     </div>
@@ -138,7 +172,7 @@
     </div> <!-- end row-->
 
     <div class="row">
-       
+
         <div class="col-lg-12">
             @session('success')
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -153,13 +187,15 @@
                         <h5 class="card-title mb-0 flex-grow-1">Categories</h5>
                         <div class="flex-shrink-0">
                             <div class="d-flex gap-2 flex-wrap">
-                                <button class="btn btn-primary" id="remove-actions" onclick="deleteMultiple()"><i class="ri-delete-bin-2-line"></i></button>
-                                <a href="{{ route('admin.brand.create') }}" class="btn btn-danger"><i class="ri-add-line align-bottom me-1"></i> Create Brand</a>
+                                <button class="btn btn-primary" id="remove-actions" onclick="deleteMultiple()"><i
+                                        class="ri-delete-bin-2-line"></i></button>
+                                <a href="{{ route('admin.brand.create') }}" class="btn btn-danger"><i
+                                        class="ri-add-line align-bottom me-1"></i> Create Brand</a>
                             </div>
                         </div>
                     </div>
                 </div>
-              
+
                 <div class="card-body">
                     <div>
                         <!-- Hoverable Rows -->
@@ -173,26 +209,37 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($brands as $key =>  $brand)
+                                @foreach ($brands as $key => $brand)
                                     <tr>
                                         <th scope="row">{{ $key + 1 }}</th>
                                         <td>{{ $brand->name }}</td>
-                                        <td>{{ $brand->logo }}</td>
+                                        <td>
+                                            <div class="d-flex gap-2 align-items-center">
+                                                <div class="flex-shrink-0">
+                                                    <img src="{{ asset('storage/' . $brand->logo) }}" alt=""
+                                                        class="avatar-xs rounded-circle">
+                                                </div>
+                                            </div>
+                                        </td>
                                         <td>
                                             <div class="hstack gap-3 flex-wrap">
-                                                <a href="{{ route('admin.brand.edit', $brand->id) }}" class="btn btn-link link-success fs-15 p-0 m-0 border-0">
+                                                <a href="{{ route('admin.brand.edit', $brand->id) }}"
+                                                    class="btn btn-link link-success fs-15 p-0 m-0 border-0">
                                                     <i class="ri-edit-2-line"></i>
                                                 </a>
-                                                
-                                                <form action="{{ route('admin.brand.destroy', $brand->id) }}" method="POST" style="display: inline;">
+
+                                                <form action="{{ route('admin.brand.destroy', $brand->id) }}"
+                                                    method="POST" style="display: inline;">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-link link-danger fs-15 p-0 m-0 border-0" onclick="return confirm('Are you sure you want to delete this brand?')">
+                                                    <button type="submit"
+                                                        class="btn btn-link link-danger fs-15 p-0 m-0 border-0"
+                                                        onclick="return confirm('Are you sure you want to delete this brand?')">
                                                         <i class="ri-delete-bin-line"></i>
                                                     </button>
                                                 </form>
                                             </div>
-                                            
+
                                         </td>
                                     </tr>
                                 @endforeach
