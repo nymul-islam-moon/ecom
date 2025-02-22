866c5b2534d37e576a04aeae3b1c955f53e6641a
Nymul Islam <towkir1997islam@gmail.com>
2025-02-22 10:26:59 +0600

    adding testing

---
+++ tests/Unit/Api/Admin/CategoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Api\Admin;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_basic_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
