b4ef37b4fe889276731aec066698bcab74d3fada
Nymul Islam <towkir1997islam@gmail.com>
2025-02-27 10:51:45 +0600

    unit testing working

--- tests/Unit/Api/Admin/CategoryTest.php
+++ tests/Unit/Api/Admin/CategoryTest.php
@@ -13,4 +13,18 @@ class CategoryTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function test_can_store_category()
+    {
+        $data = [
+            'name' => 'Test Category',
+        ];
+
+        $response = $this->postJson(route('categories.store'), $data);
+
+        $response->assertStatus(201)
+            ->assertJsonFragment(['name' => 'Test Category']);
+
+        $this->assertDatabaseHas('categories', ['name' => 'Test Category']);
+    }
 }
